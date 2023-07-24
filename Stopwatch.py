67529e224ee9d7dd2866910330c3fde08b9deb95
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-07-24 12:57:30 -0400

    + timming code

---
+++ Stopwatch.py
@@ -0,0 +1,51 @@
+import datetime
+import statistics
+
+class TimerStats:
+    def __init__(self):
+        self.times = []
+        self.start_time = None
+
+    def start(self):
+        self.start_time = datetime.datetime.now()
+
+    def stop(self):
+        if self.start_time is not None:
+            elapsed_time = datetime.datetime.now() - self.start_time
+            self.times.append(elapsed_time.total_seconds())
+            self.start_time = None
+            return elapsed_time
+
+    def get_stats(self):
+        if len(self.times) > 0:
+            return {
+                'last': self.times[-1],
+                'average': statistics.mean(self.times),
+                'min': min(self.times),
+                'max': max(self.times),
+                'std_dev': statistics.stdev(self.times) if len(self.times) > 1 else 0
+            }
+        else:
+            return None
+
+class Stopwatch:
+    def __init__(self, debug=True):
+        self.timers = {}
+        self.debug = debug
+        
+    def start(self, key):
+        if self.debug:
+            print("Starting ", key)
+        if key not in self.timers:
+            self.timers[key] = TimerStats()
+        self.timers[key].start()
+
+    def stop(self, key):
+        if key in self.timers:
+            et = self.timers[key].stop()
+            stuff = self.timers[key].get_stats()
+            if self.debug:
+                print(f"Stopping {key} which ran for {et} && {stuff}")
+            return stuff
+        else:
+            return None
--- tracribe.py
+++ tracribe.py
@@ -5,13 +5,17 @@ import wave
 import whisper
 from PyQt5.QtWidgets import QApplication, QPushButton
 from PyQt5.QtCore import QThread, pyqtSignal
+from Stopwatch import Stopwatch
+
+sw = Stopwatch()
 
 # Variables for model size
-model_size = "small"  # Other options: "tiny", "small", "medium", "large"
+model_size = "medium"  # Other options: "tiny", "small", "medium", "large"
 
 # Load the Whisper model
-print("Loading Whisper model...")
+sw.start("Loading Whisper model")
 model = whisper.load_model(model_size)
+sw.stop("Loading Whisper model")
 
 # Create an InputStream for the microphone
 stream = sd.InputStream(samplerate=16000, channels=1, dtype='float32')
@@ -37,27 +41,34 @@ recording_thread = RecordingThread()
 # Define the callback for the button press event
 def on_button_press():
     global buffer
+    sw.start("Recording")
     buffer = np.array([], dtype='float32')  # Clear the buffer
     recording_thread.start()  # Start the RecordingThread
 
 # Define the callback for the button release event
 def on_button_release():
     recording_thread.terminate()  # Stop the RecordingThread
+    rt = sw.stop("Recording")["last"]
 
     global buffer
     # Convert the buffer to bytes
     audio_data = np.int16(buffer * 32767).tobytes()
 
+    sw.start("Saving")
     # Save the audio data to a temporary WAV file
     with wave.open('temp.wav', 'wb') as wf:
         wf.setnchannels(1)
         wf.setsampwidth(2)
         wf.setframerate(16000)
         wf.writeframes(audio_data)
+    sw.stop("Saving")
 
+    sw.start("Transcribing")
     # Transcribe the audio data with Whisper
-    print("Transcribing audio...")
     result = model.transcribe('temp.wav')
+    tt = sw.stop("Transcribing")["last"]
+    
+    print(f"Seconds per seconds {rt/tt}")
     print(result['text'])
 
 # Create a PyQt application and button
