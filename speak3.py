fc29a18e459f1107cbb4f4818c4d567829f10950
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-03 13:20:39 -0400

    moved microsoft model to gpu if available

--- speak3.py
+++ speak3.py
@@ -2,20 +2,29 @@ from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5Hif
 from datasets import load_dataset
 import torch
 import soundfile as sf
-from datasets import load_dataset
+
+# Check if CUDA is available and set the device to GPU if it is
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device: {device}')  # This will print 'cuda' if CUDA is available
 
 processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
-model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
-vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts").to(device)  # Move the model to the GPU
+vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan").to(device)  # Move the vocoder to the GPU
 
 # load xvector containing speaker's voice characteristics from a dataset
 embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
-speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0).to(device)  # Move the embeddings to the GPU
 
 print("Running...")
-inputs = processor(text="Hello! I am a computer. Do you understand english like I do? If so, can you see how well I am handling punctuation? What about my pauses? Or my grammar?", return_tensors="pt")
+inputs = processor(text="Hello! I am a computer. Do you understand english like I do? If so, can you see how well I am handling punctuation? What about my pauses? Or my grammar? What if I am running on the Gee Pee yoU versus the (C) (P) (U)? Do you still understand me then? That would be good if you did because this thing is fairly slow to run on the [C] [P] [U]. I still as a computer however do not do well as you can see reading acrynyms, things like See, pea, you... arent read well when you type CPU.", return_tensors="pt")
+
+# Move the inputs to the GPU
+inputs = {name: tensor.to(device) for name, tensor in inputs.items()}
 
 speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+
+# Move the speech tensor back to CPU for saving to file
+speech = speech.cpu()
 
 sf.write("speech.wav", speech.numpy(), samplerate=16000)
 print("Done")
