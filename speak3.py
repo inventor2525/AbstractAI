e832f4967e41b7b4af61b7bc6fff1d7c1ea14f3e
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-03 13:52:17 -0400

    made text to speach into a class

--- speak3.py
+++ speak3.py
@@ -4,48 +4,45 @@ import torch
 import soundfile as sf
 from Stopwatch import Stopwatch
 
-sw = Stopwatch()
+class TextToSpeech:
+    def __init__(self):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.sw = Stopwatch()
 
-sw.start("load text to speach")
-# Check if CUDA is available and set the device to GPU if it is
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f'Using device: {device}')  # This will print 'cuda' if CUDA is available
+    def text_to_speech(self, text:str, file_path:str):
+        raise NotImplementedError
 
-processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
-model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts").to(device)  # Move the model to the GPU
-vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan").to(device)  # Move the vocoder to the GPU
+class MicrosoftSpeechT5TTS(TextToSpeech):
+    def __init__(self):
+        super().__init__()
+        self.sw.start("load text to speech")
+        self.processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+        self.model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts").to(self.device)
+        self.vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan").to(self.device)
+        embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+        self.speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0).to(self.device)
+        self.sw.stop("load text to speech")
 
-# load xvector containing speaker's voice characteristics from a dataset
-embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
-speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0).to(device)  # Move the embeddings to the GPU
+    def text_to_speech(self, text:str, file_path:str="speech.wav"):
+        self.sw.start("text to speech")
+        inputs = self.processor(text=text, return_tensors="pt")
+        inputs = {name: tensor.to(self.device) for name, tensor in inputs.items()}
+        speech = self.model.generate_speech(inputs["input_ids"], self.speaker_embeddings, vocoder=self.vocoder)
+        speech = speech.cpu()
+        sf.write(file_path, speech.numpy(), samplerate=16000)
+        self.sw.stop("text to speech")
 
-sw.stop("load text to speach")
+tts = MicrosoftSpeechT5TTS()
 
-def text_to_speach(text:str, file_path:str="speech.wav"):
-    sw.start("text to speach")
-    inputs = processor(text=text, return_tensors="pt")
-
-    # Move the inputs to the GPU
-    inputs = {name: tensor.to(device) for name, tensor in inputs.items()}
-
-    speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
-
-    # Move the speech tensor back to CPU for saving to file
-    speech = speech.cpu()
-
-    sf.write(file_path, speech.numpy(), samplerate=16000)
-    sw.stop("text to speach")
-
-text_to_speach(
+tts.text_to_speech(
     "Hello! I am a computer. Do you understand english like I do? If so, can you see how well I am handling punctuation? What about my pauses? Or my grammar?",
     "file_1.wav"
 )
-text_to_speach(
+tts.text_to_speech(
     "What if I am running on the Gee Pee yoU versus the (C) (P) (U)? Do you still understand me then? That would be good if you did because this thing is fairly slow to run on the [C] [P] [U]. ",
     "file_2.wav"
 )
-
-text_to_speach(
+tts.text_to_speech(
     "I still as a computer however, do not do well, as you can see, reading acrynyms. Things like See, pea, you? arent read well when you type CPU.",
     "file_3.wav"
 )
