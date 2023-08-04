7ceb4059dd48e3ed4bbc92a90677b968acea8088
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-04 19:29:21 -0400

    implemented better timing code

--- LLMs/OpenAssistant.py
+++ LLMs/OpenAssistant.py
@@ -32,34 +32,76 @@ class LLM:
 		output = self.model.generate(**inputs, do_sample=True, top_p=0.95, top_k=0, max_new_tokens=1024)
 		return output
 
-	def timed_prompt(self, prompt: str):
-		start_time = datetime.now()
-		output = self.respond(prompt)
-		end_time = datetime.now()
-		generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
-		duration = end_time - start_time
-		duration_seconds = duration.total_seconds()
-		token_count = len(output[0])
-		chars_per_second = len(generated_text) / duration_seconds
-		tokens_per_second = token_count / duration_seconds
-		print(f"Start time: {start_time}")
-		print(f"End time: {end_time}")
-		print(f"Duration: {duration_seconds} seconds")
-		print(f"Response length: {len(generated_text)} characters")
-		print(f"Token count: {token_count}")
-		print(f"Characters per second: {chars_per_second}")
-		print(f"Tokens per second: {tokens_per_second}")
-		return generated_text
+class AMMS:
+	def __init__(self):
+		self.values = []
+	
+	def add(self, value):
+		self.values.append(value)
+	
+	def current(self):
+		return self.values[-1]
+	
+	def average(self):
+		return np.mean(self.values)
+	
+	def min(self):
+		return np.min(self.values)
+	
+	def max(self):
+		return np.max(self.values)
+	
+	def std(self):
+		return np.std(self.values)
+	
+	def __str__(self):
+		return f"current={self.current():.2f} average={self.average():.2f} min={self.min():.2f} max={self.max():.2f} std={self.std():.2f}"
 
+class LLMStats:
+	def __init__(self):
+		self.duration = AMMS()
+		self.response_length = AMMS()
+		self.token_count = AMMS()
+		self.chars_per_second = AMMS()
+		self.tokens_per_second = AMMS()
+		
+	def print(self):
+		print(f"Duration: {self.duration}")
+		print(f"Response length: {self.response_length}")
+		print(f"Token count: {self.token_count}")
+		print(f"Characters per second: {self.chars_per_second}")
+		print(f"Tokens per second: {self.tokens_per_second}")
+		
 class OpenAssistantLLM(LLM):
 	def __init__(self, model_name: str):
 		super().__init__()
 		self.model_name = model_name
+		self.stats = LLMStats()
 
 	def start(self):
-		print(f"Loading LLM \"{self.model_name}\"")
+		print(f"\n\n\nLoading LLM \"{self.model_name}\"...\n\n\n")
 		self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, use_fast=False)
 		self.model = AutoModelForCausalLM.from_pretrained(self.model_name, torch_dtype=torch.float16, low_cpu_mem_usage=True, device_map="auto")
+
+	def timed_prompt(self, prompt: str):
+		start_time = datetime.now()
+		output = self.respond(prompt)
+		generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
+		end_time = datetime.now()
+		
+		print(f"Start time: {start_time}")
+		print(f"End time: {end_time}")
+		
+		duration_seconds = (end_time - start_time).total_seconds()
+		token_count = len(output[0])
+		
+		self.stats.duration.add(duration_seconds)
+		self.stats.response_length.add(len(generated_text))
+		self.stats.token_count.add(token_count)
+		self.stats.chars_per_second.add(len(generated_text) / duration_seconds)
+		self.stats.tokens_per_second.add(token_count / duration_seconds)
+		self.stats.print()
+		return generated_text
 
 class PromptGenerator:
 	def __init__(self, system_message: str):
