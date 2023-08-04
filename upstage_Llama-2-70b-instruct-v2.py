e89d762daca8847d5466277a2885bfbda4a6e961
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-04 08:30:30 -0400

    fix for upstage lambda (slow af)

--- upstage_Llama-2-70b-instruct-v2.py
+++ upstage_Llama-2-70b-instruct-v2.py
@@ -2,18 +2,18 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 
-tokenizer = AutoTokenizer.from_pretrained("upstage/Llama-2-70b-instruct-v2")
+tokenizer = AutoTokenizer.from_pretrained("upstage/Llama-2-70b-instruct-v2", use_fast=False)
 model = AutoModelForCausalLM.from_pretrained(
     "upstage/Llama-2-70b-instruct-v2",
     device_map="auto",
     torch_dtype=torch.float16,
-    load_in_8bit=True,
+    # load_in_8bit=True,
     rope_scaling={"type": "dynamic", "factor": 2} # allows handling of longer inputs
 )
 
 prompt = "### User:\nThomas is healthy, but he has to go to the hospital. What could be the reasons?\n\n### Assistant:\n"
 inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
-del inputs["token_type_ids"]
+# del inputs["token_type_ids"]
 streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
 output = model.generate(**inputs, streamer=streamer, use_cache=True, max_new_tokens=float('inf'))
