e51c81d153d60b51cb35016f2fdf303a0e47314f
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-04 20:08:37 -0400

    script to test many openassistant models

---
+++ LLMs/test_OpenAssist.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get the start time
+start_time=$(date +"%Y%m%d%H%M%S")
+
+# Define the log file
+log_file="log_${start_time}.txt"
+
+# List of model names
+model_names=(
+"OpenAssistant/falcon-7b-sft-mix-2000"
+"OpenAssistant/llama2-13b-orca-v2-8k-3166"
+"OpenAssistant/oasst-sft-1-pythia-12b"
+"OpenAssistant/oasst-rm-2-pythia-6.9b-epoch-1"
+"OpenAssistant/falcon-40b-sft-mix-1226" 
+"OpenAssistant/falcon-40b-sft-top1-560" 
+)
+
+# Loop over each model name and call the Python script
+for model_name in "${model_names[@]}"
+do
+  python your_script.py --model_name $model_name | tee -a $log_file
+done
