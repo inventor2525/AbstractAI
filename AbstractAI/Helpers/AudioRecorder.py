96dedd8810f534aa462fbe81d4ed447d00968a12
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-09 19:38:51 -0400

    fixed recorder threading error

--- AbstractAI/Helpers/AudioRecorder.py
+++ AbstractAI/Helpers/AudioRecorder.py
@@ -8,48 +8,47 @@ class AudioRecorder:
 	def __init__(self):
 		self.stream = sd.InputStream(samplerate=16000, channels=1, dtype='float32')
 		self.recording_thread = self.RecordingThread(self)
-		self.buffer = np.array([], dtype='float32')
+		self.lock = threading.Lock()
+		self.recording_thread.start()
 
 	class RecordingThread(threading.Thread):
 		def __init__(self, recorder):
 			super().__init__()
-			self.running = False
-			self.lock = threading.Lock()
+			self.record = False
 			self.recorder = recorder
+			self.temporary_buffer = np.array([], dtype='float32')
 
 		def run(self):
-			self.running = True
 			print("...........starting thread............")
 			self.recorder.stream.start()
-			while self.running:
+			while True:
 				data, _ = self.recorder.stream.read(1024)
-				with self.lock:
-					self.recorder.buffer = np.append(self.recorder.buffer, data)
-			self.recorder.stream.stop()
-			print("...........stopping thread............")
-
-		def stop(self):
-			with self.lock:
-				self.running = False
-			self.recorder.stream.stop()
+				if self.record:
+					self.temporary_buffer = np.append(self.temporary_buffer, data)
+				else:
+					self.temporary_buffer = data
 
 	def start_recording(self):
-		Stopwatch.singleton.start("Recording")
-		self.buffer = np.array([], dtype='float32')
-		self.recording_thread.start()
+		with self.lock:
+			Stopwatch.singleton.start("Recording")
+			self.recording_thread.record = True
+			self.buffer = np.array([], dtype='float32')
 
 	def stop_recording(self):
-		self.recording_thread.stop()
-		self.last_record_time = Stopwatch.singleton.stop("Recording")["last"]
-		audio_data = np.int16(self.buffer * 32767).tobytes()
+		with self.lock:
+			self.recording_thread.record = False
+			self.buffer = self.recording_thread.temporary_buffer
+			self.recording_thread.temporary_buffer = np.array([], dtype='float32')
+			self.last_record_time = Stopwatch.singleton.stop("Recording")["last"]
+			audio_data = np.int16(self.buffer * 32767).tobytes()
 
-		Stopwatch.singleton.start("Saving")
-		audio_segment = AudioSegment(
-			data=audio_data,
-			sample_width=2,
-			frame_rate=16000,
-			channels=1
-		)
-		Stopwatch.singleton.stop("Saving")
-		
-		return audio_segment
+			Stopwatch.singleton.start("Saving")
+			audio_segment = AudioSegment(
+				data=audio_data,
+				sample_width=2,
+				frame_rate=16000,
+				channels=1
+			)
+			Stopwatch.singleton.stop("Saving")
+
+			return audio_segment
