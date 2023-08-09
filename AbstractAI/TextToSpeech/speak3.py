7c37f2db1601fb03cd4a82745f50c96435ab92a7
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-09 12:02:21 -0400

    oops

--- AbstractAI/TextToSpeech/speak3.py
+++ AbstractAI/TextToSpeech/speak3.py
@@ -1,7 +1,7 @@
 from AbstractAI.Helpers.AudioPlayer import *
 from AbstractAI.TextToSpeech.MicrosoftTTS import *
 
-ap = AudioPlayer()
+# ap = AudioPlayer()
 
 tts = MicrosoftSpeechT5_TTS()
 
@@ -9,16 +9,16 @@ s1 = tts.text_to_speech(
     "Hello! I am a computer. Do you understand english like I do? If so, can you see how well I am handling punctuation? What about my pauses? Or my grammar?"
 )
 s1.export("file_1.wav", format="wav")
-ap.play(s1)
+# ap.play(s1)
 
 s2 = tts.text_to_speech(
     "What if I am running on the Gee Pee yoU versus the (C) (P) (U)? Do you still understand me then? That would be good if you did because this thing is fairly slow to run on the [C] [P] [U]. "
 )
 s2.export("file_2.wav", format="wav")
-ap.play(s2)
+# ap.play(s2)
 
 s3 = tts.text_to_speech(
     "I still as a computer however, do not do well, as you can see, reading acrynyms. Things like See, pea, you? arent read well when you type CPU."
 )
 s3.export("file_3.wav", format="wav")
-ap.play(s3)
+# ap.play(s3)
