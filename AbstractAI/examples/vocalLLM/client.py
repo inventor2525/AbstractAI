8c39ca311aee1ae361bb242f8c68664486685146
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-09 21:25:43 -0400

    integrated LLM into the client server app

--- AbstractAI/examples/vocalLLM/client.py
+++ AbstractAI/examples/vocalLLM/client.py
@@ -1,32 +1,75 @@
+import sys
 import argparse
-from PyQt5.QtWidgets import QApplication, QPushButton
+import requests
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QLabel, QPushButton, QHBoxLayout
 from AbstractAI.Helpers.AudioRecorder import AudioRecorder
 from AbstractAI.SpeechToText.RemoteSTT import RemoteSTT
 
-def on_button_press():
-	recorder.start_recording()
+class Application:
+	def __init__(self, host, port):
+		self.app = QApplication(sys.argv)
+		
+		self.host = host
+		self.port = port
+		
+		self.recorder = AudioRecorder()
+		self.stt = RemoteSTT(host, port)
+		
+		self.gui_init()
+		sys.exit(self.app.exec_())
 
-def on_button_release():
-	file_name = 'temp.wav'
-	
-	audio_segment = recorder.stop_recording()
-	audio_segment.export(file_name, format="wav")
-	result = remote_stt.transcribe_str(file_name)
-	
-	print(f"Transcribed audio at {recorder.last_record_time/remote_stt.last_transcription_time} Seconds per second. Which returned: '{result}'.")
+	def on_record_button_press(self):
+		self.recorder.start_recording()
 
-parser = argparse.ArgumentParser(description='Remote Speech-to-Text Client')
-parser.add_argument('host', type=str, help='The server\'s IP address or hostname (e.g., \'localhost\' or \'0.0.0.0\').')
-parser.add_argument('port', type=int, help='The port number on which the server is running (e.g., 8000).')
-args = parser.parse_args()
+	def on_record_button_release(self):
+		file_name = 'temp.wav'
+		audio_segment = self.recorder.stop_recording()
+		audio_segment.export(file_name, format="wav")
+		result = self.stt.transcribe_str(file_name)
+		self.you_text_edit.setText(result)
 
-recorder = AudioRecorder()
-remote_stt = RemoteSTT(args.host, args.port)
+	def on_send_button_click(self):
+		user_text = self.you_text_edit.toPlainText()
+		response = requests.post(f'{self.host}:{self.port}/llm', json={'text': user_text})
+		self.ai_response_text_edit.setText(response.json()['response'])
 
-app = QApplication([])
-button = QPushButton('Hold to record')
-button.pressed.connect(on_button_press)
-button.released.connect(on_button_release)
-button.show()
+	def gui_init(self):
+		window = QWidget()
+		layout = QVBoxLayout()
 
-app.exec_()
+		you_label = QLabel("You:")
+		layout.addWidget(you_label)
+		self.you_text_edit = QTextEdit()
+		layout.addWidget(self.you_text_edit)
+
+		space_label = QLabel()
+		space_label.setFixedHeight(20)
+		layout.addWidget(space_label)
+
+		ai_response_label = QLabel("AI Response:")
+		layout.addWidget(ai_response_label)
+		self.ai_response_text_edit = QTextEdit()
+		self.ai_response_text_edit.setReadOnly(True)
+		layout.addWidget(self.ai_response_text_edit)
+
+		buttons_layout = QHBoxLayout()
+		record_button = QPushButton('Hold to record')
+		record_button.pressed.connect(self.on_record_button_press)
+		record_button.released.connect(self.on_record_button_release)
+		buttons_layout.addWidget(record_button)
+		send_button = QPushButton('Send')
+		send_button.clicked.connect(self.on_send_button_click)
+		buttons_layout.addWidget(send_button)
+
+		layout.addLayout(buttons_layout)
+		window.setLayout(layout)
+		window.show()
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Remote Speech-to-Text Client')
+	parser.add_argument('host', type=str, help='The server\'s IP address or hostname (e.g., \'localhost\' or \'0.0.0.0\').')
+	parser.add_argument('port', type=int, help='The port number on which the server is running (e.g., 8000).', default=8000)
+	args = parser.parse_args()
+
+	Application(args.host, args.port)
--- AbstractAI/examples/vocalLLM/server.py
+++ AbstractAI/examples/vocalLLM/server.py
@@ -1,13 +1,16 @@
 import argparse
 from flask import Flask, request, jsonify
 from AbstractAI.SpeechToText.WhisperSTT import WhisperSTT
+from AbstractAI.LLMs.LoadLLM import LoadLLM
 
 parser = argparse.ArgumentParser(description='Remote Speech-to-Text Server')
 parser.add_argument('model_name', type=str, choices=['tiny', 'tiny.en', 'small', 'small.en', 'medium', 'medium.en', 'large'], help='Whisper model size to use for transcription.')
+parser.add_argument('llm_name', type=str, help='The LLM name to use.', default='stabilityai/StableBeluga2-7B')
 args = parser.parse_args()
 
 app = Flask(__name__)
 stt = WhisperSTT(args.model_name)
+llm, _ = LoadLLM(args.llm_name)
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
@@ -16,5 +19,11 @@ def transcribe():
 	audio_file.save(file_name)
 	transcription = stt.transcribe_str(file_name)
 	return jsonify({'transcription': transcription})
+
+@app.route('/llm', methods=['POST'])
+def llm_endpoint():
+	text = request.json['text']
+	response = llm.timed_prompt(text)
+	return jsonify({'response': response})
 
 app.run(host='0.0.0.0', port=8000)
