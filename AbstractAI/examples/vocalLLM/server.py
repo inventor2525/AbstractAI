a19128ca5963e6f439f487404ad254669ad651d7
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-10 17:19:19 -0400

    again

--- AbstractAI/examples/vocalLLM/server.py
+++ AbstractAI/examples/vocalLLM/server.py
@@ -4,8 +4,8 @@ from AbstractAI.SpeechToText.WhisperSTT import WhisperSTT
 from AbstractAI.LLMs.LoadLLM import LoadLLM
 
 parser = argparse.ArgumentParser(description='Remote Speech-to-Text Server')
-parser.add_argument('model_name', type=str, choices=['tiny', 'tiny.en', 'small', 'small.en', 'medium', 'medium.en', 'large'], help='Whisper model size to use for transcription.', default='medium.en')
-parser.add_argument('llm_name', type=str, help='The LLM name to use.', default='stabilityai/StableBeluga2-7B', nargs='?')
+parser.add_argument('--model_name', type=str, choices=['tiny', 'tiny.en', 'small', 'small.en', 'medium', 'medium.en', 'large'], help='Whisper model size to use for transcription.', default='medium.en')
+parser.add_argument('--llm_name', type=str, help='The LLM name to use.', default='stabilityai/StableBeluga2-7B')
 args = parser.parse_args()
 
 app = Flask(__name__)
