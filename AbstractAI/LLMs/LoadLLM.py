1608bd0a83f336085d95ba6d4f34ff93d0a0a8ef
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-10 19:22:30 -0400

    + debug

--- AbstractAI/LLMs/LoadLLM.py
+++ AbstractAI/LLMs/LoadLLM.py
@@ -4,13 +4,16 @@ from typing import Tuple
 
 def LoadLLM(model_name:str, system_message:str) -> Tuple[LLM, PromptGenerator]:
 	print(f"Loading LLM '{model_name}' with system message '{system_message}'")
+	llm, generator = (None, None)
 	if model_name.startswith("OpenAssistant"):
-		return (
+		llm, generator = (
 			OpenAssistantLLM(model_name),
 			OpenAssistantPromptGenerator(system_message)
 		)
 	if model_name.startswith("stabilityai/StableBeluga2"):
-		return (
+		llm, generator = (
 			StableBeluga2(model_name),
 			StableBeluga2PromptGenerator(system_message)
 		)
+	print(llm, generator)
+	return (llm, generator)
