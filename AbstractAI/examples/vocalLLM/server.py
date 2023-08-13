9ef65be156ada399dc4079356fafaf99ed9eb927
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-13 15:48:40 -0400

    fix default model name

--- AbstractAI/examples/vocalLLM/server.py
+++ AbstractAI/examples/vocalLLM/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 
 parser = argparse.ArgumentParser(description='Remote Speech-to-Text Server')
 parser.add_argument('--model_name', type=str, choices=['tiny', 'tiny.en', 'small', 'small.en', 'medium', 'medium.en', 'large'], help='Whisper model size to use for transcription.', default='medium.en', required=False)
-parser.add_argument('--llm_name', type=str, help='The LLM name to use.', default='stabilityai/StableBeluga2-7B', required=False)
+parser.add_argument('--llm_name', type=str, help='The LLM name to use.', default='stabilityai/StableBeluga-7B', required=False)
 args = parser.parse_args()
 
 app = Flask(__name__)
