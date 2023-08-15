7c246a3230c65ec4a9256bedea8e3117c79e9c0e
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-15 18:05:32 -0400

    improved terminal formatting and added demo

--- AbstractAI/Helpers/Terminal.py
+++ AbstractAI/Helpers/Terminal.py
@@ -1,52 +1,47 @@
+import pexpect
 import asyncio
-import os
-import pty
-import signal
-import subprocess
 import re
 
 class Terminal:
 	def __init__(self):
-		self.master, self.slave = pty.openpty()
-		self.process = subprocess.Popen(
-			["bash"],
-			stdin=self.slave,
-			stdout=self.slave,
-			stderr=self.slave,
-			bufsize=0,
-			preexec_fn=os.setsid,
-		)
+		self.child = pexpect.spawn('/bin/bash', encoding='utf-8')
+		self.output_history = ''
+		self.child.expect(['\$', '#'], timeout=None)
+		self.run_command_blocking('pwd')  # Get the working directory at start
 		
-	async def _read_output(self):
-		"""Clean the output from the terminal."""
-		loop = asyncio.get_event_loop()
-		output = await loop.run_in_executor(None, os.read, self.master, 1024)
-		decoded_output = output.decode()
-		ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-		cleaned_output = ansi_escape.sub('', decoded_output)
-		extra_characters = re.compile(r'(\x07|^0;[^$]*)(?=.*?@)')
-		return extra_characters.sub('', cleaned_output)
+		response = self.output_history.strip().split('\r\n')[2]
+		self.output_history = response + '\n'
 		
-	async def run_command(self, command:str, timeout:float=None)->str:
-		"""Run the passed command and return the output."""
+	async def run_command(self, command, timeout:float=None):
+		self.child.sendline(command)
+		self.child.expect(['\$ '], timeout=timeout)
+		response = self.child.before
+		response = re.sub(r'[^\x00-\x7F]+', '', response)  # Remove non-ASCII characters
+		response = re.sub(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])', '', response)  # Remove escape sequences
 		
-		os.write(self.master, f"{command}\n".encode())
-
-		lines = []
-		while True:
-			try:
-				line = await asyncio.wait_for(self._read_output(), timeout)
-				if line:
-					lines.append(line.rstrip())
-				else:
-					break
-			except asyncio.TimeoutError:
-				print("Command timed out")
-				await self.stop()
-				break
-
-		return "\n".join(lines)
-
+		response = re.sub(r'0;(.+?: .+)\x07', '', response)
+		response = f"{response}$ "
+		self.output_history += response  # Append to output history
+		return response
+	
+	def run_command_blocking(self, command, timeout:float=None):
+		return asyncio.run(self.run_command(command, 2))
+		
 	async def stop(self):
 		"""Stop any running command"""
-		os.killpg(os.getpgid(self.process.pid), signal.SIGINT)
+		os.killpg(os.getpgid(self.child.pid), signal.SIGINT)
+
+# Example usage
+if __name__ == '__main__':
+	terminal = Terminal()
+	print(terminal.output_history)
+	with open('terminal_log.txt', 'a') as file:
+		file.write(terminal.output_history)
+		
+	while True:
+		user_input = input()
+		response = terminal.run_command_blocking(user_input)
+		print(response)
+		
+		with open('terminal_log.txt', 'a') as file:
+			file.write(response)
