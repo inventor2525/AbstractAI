c87493d73b4c5a74eee20d19f8101da88832ca4b
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-08-16 01:24:04 -0400

    Terminal stability improvements & docker support

--- AbstractAI/Helpers/Terminal.py
+++ AbstractAI/Helpers/Terminal.py
@@ -3,29 +3,40 @@ import asyncio
 import re
 
 class Terminal:
-	def __init__(self):
-		self.child = pexpect.spawn('/bin/bash', encoding='utf-8')
+	def __init__(self, docker_container_name:str=None):
+		command = "/bin/bash"
+		if docker_container_name is not None:
+			command = f"docker run -it {docker_container_name} /bin/bash"
 		self.output_history = ''
-		self.child.expect(['\$', '#'], timeout=None)
-		self.run_command_blocking('pwd')  # Get the working directory at start
+		self.pattern = r'([a-zA-Z0-9_.-]+)@([a-zA-Z0-9_.-]+):([^$#]*)[#$] '
 		
-		response = self.output_history.strip().split('\r\n')[2]
-		self.output_history = response + '\n'
+		self.child = pexpect.spawn(command, encoding='utf-8')
+		self.output_history = self._get_output()
+		
+	def _clean_response(self, text:str)->str:
+		text = re.sub(r'[^\x00-\x7F]+', '', text)  # Remove non-ASCII characters
+		text = re.sub(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])', '', text)  # Remove escape sequences
+		
+		# Fix the "user@computer:PWD$ ":
+		text = re.sub(r'0;(.+?: .+)\x07', '', text)
+		return text
+			
+	def _get_output(self, timeout:float=None):
+		self.child.expect([self.pattern], timeout=timeout)
+		
+		return self._clean_response(
+			self.child.before + self.child.match.group(0)
+		).replace('\r\n', '\n')
 		
 	async def run_command(self, command, timeout:float=None):
 		self.child.sendline(command)
-		self.child.expect(['\$ '], timeout=timeout)
-		response = self.child.before
-		response = re.sub(r'[^\x00-\x7F]+', '', response)  # Remove non-ASCII characters
-		response = re.sub(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])', '', response)  # Remove escape sequences
+		response = self._get_output()
 		
-		response = re.sub(r'0;(.+?: .+)\x07', '', response)
-		response = f"{response}$ "
-		self.output_history += response  # Append to output history
-		return response
+		self.output_history += response
+		return response.removeprefix(f"{command}\n")
 	
 	def run_command_blocking(self, command, timeout:float=None):
-		return asyncio.run(self.run_command(command, 2))
+		return asyncio.run(self.run_command(command, 30))
 		
 	async def stop(self):
 		"""Stop any running command"""
@@ -33,15 +44,15 @@ class Terminal:
 
 # Example usage
 if __name__ == '__main__':
+	def log_print(user_input:str, response:str):
+		print(response)
+		with open('terminal_log.txt', 'a') as file:
+			file.write(f"{user_input}\n{response}")
+	
 	terminal = Terminal()
-	print(terminal.output_history)
-	with open('terminal_log.txt', 'a') as file:
-		file.write(terminal.output_history)
-		
+	log_print("", terminal.output_history)
+	
 	while True:
 		user_input = input()
 		response = terminal.run_command_blocking(user_input)
-		print(response)
-		
-		with open('terminal_log.txt', 'a') as file:
-			file.write(response)
+		log_print(user_input, response)
