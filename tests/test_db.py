f8f26103b6f44289d41e5010e0b867a9e0580fad
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-09-02 17:25:13 -0400

    + db test

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,26 +1,96 @@
 import unittest
-from AbstractAI.Conversation import Conversation, Message, MessageSequence
-from AbstractAI.Conversation.MessageSources import UserSource, TerminalSource, EditSource
+from AbstractAI.Conversation import *
 from AbstractAI.DB.Database import Database
-
+from typing import Callable
 # Compare functions
+def both_none_or_condition(obj1, obj2, condition:Callable[[object, object], bool]) -> bool:
+    if obj1 is None:
+        if obj2 is None:
+            return True
+        else:
+            return False
+    if obj2 is None:
+        return False
+    return condition(obj1,obj2)
+    
+def compare_hashable(obj1, obj2):
+    if not both_none_or_condition(obj1,obj2, lambda o1, o2: o1.hash == o2.hash):
+        return False
+    return True
 
 def compare_conversations(conv1, conv2):
-    # Add comparison logic for Conversation objects
-    pass
+    if not compare_hashable(conv1, conv2):
+        return False
+    if conv1.creation_time != conv2.creation_time:
+        return False
+    if conv1.name != conv2.name:
+        return False
+    if conv1.description != conv2.description:
+        return False
+    if not compare_hashable(conv1.message_sequence, conv2.message_sequence):
+        return False
+    return True
 
 def compare_messages(msg1, msg2):
-    # Add comparison logic for Message objects
-    pass
+    if not compare_hashable(msg1, msg2):
+        return False
+    if msg1.creation_time != msg2.creation_time:
+        return False
+    if msg1.content != msg2.content:
+        return False
+    if msg1.role != msg2.role:
+        return False
+    if not compare_hashable(msg1.source, msg2.source):
+        return False
+    if not compare_hashable(msg1.prev_message, msg2.prev_message):
+        return False
+    if not compare_hashable(msg1.conversation, msg2.conversation):
+        return False
+    return True
 
 def compare_message_sequences(ms1, ms2):
-    # Add comparison logic for MessageSequence objects
-    pass
+    if not compare_hashable(ms1, ms2):
+        return False
+    if len(ms1.messages) != len(ms2.messages):
+        return False
+    for msg1, msg2 in zip(ms1.messages, ms2.messages):
+        if msg1.hash != msg2.hash:
+            return False
+    return True
 
 def compare_sources(src1, src2):
-    # Add comparison logic for source types
-    pass
-
+    if not compare_hashable(src1, src2):
+        return False
+    if src1.__class__ != src2.__class__:
+        return False
+    if isinstance(src1, UserSource):
+        if src1.user_name != src2.user_name:
+            return False
+        return True
+    if isinstance(src1, TerminalSource):
+        if src1.command != src2.command:
+            return False
+        return True
+    if isinstance(src1, EditSource):
+        if not compare_hashable(src1.original, src2.original):
+            return False
+        if not compare_hashable(src1.new, src2.new):
+            return False
+        return True
+    if isinstance(src1, ModelSource):
+        if src1.class_name != src2.class_name:
+            return False
+        if src1.model_name != src2.model_name:
+            return False
+        if src1.other_parameters != src2.other_parameters:
+            return False
+        if not compare_hashable(src1.message_sequence, src2.message_sequence):
+            return False
+        if not both_none_or_condition(src1.models_serialized_raw_output, src2.models_serialized_raw_output, lambda o1, o2: o1 == o2):
+            return False
+        return True
+    return False
+    
 # Test class
 
 class TestDB(unittest.TestCase):
