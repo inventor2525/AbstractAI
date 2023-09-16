cc483db1060829c1dce76efec55e85c5550a3d67
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-09-15 20:11:55 -0400

    + message view example to test with

---
+++ AbstractAI/examples/chatui_example.py
@@ -0,0 +1,26 @@
+from AbstractAI.UI.Support.MessageView import *
+
+#create a QT window with a message view in it:
+app = QApplication(sys.argv)
+
+message = Message("Hello, world!", Role.User, UserSource("User"))
+message_view = MessageView(message)
+
+
+#create a window:
+window = QWidget()
+window.setWindowTitle("Message View Test")
+window.resize(800, 600)
+
+#create a layout:
+layout = QVBoxLayout()
+window.setLayout(layout)
+
+#add the message view to the layout:
+layout.addWidget(message_view)
+
+#show the window:
+window.show()
+
+#run the QT event loop:
+sys.exit(app.exec_())
