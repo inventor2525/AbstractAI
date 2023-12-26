d1952dd5159e6272891811a47a657c314fc88239
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-12-26 13:20:43 -0500

    wip chunked transcription

--- AbstractAI/SpeechToText/ChunkedTranscription.py
+++ AbstractAI/SpeechToText/ChunkedTranscription.py
@@ -18,10 +18,10 @@ class TranscriptionState:
 		return self.fixed_transcription + self.living_transcription
 
 class ChunkedTranscription:
-	def __init__(self, tiny_model: SpeechToText, large_model: SpeechToText, consensus_threshold_seconds: int = 2):
+	def __init__(self, tiny_model: SpeechToText, large_model: SpeechToText, consensus_threshold_seconds: float = 2):
 		self.tiny_model = tiny_model
 		self.large_model = large_model
-		self.consensus_threshold = consensus_threshold_seconds * 1000  # Convert to milliseconds
+		self.consensus_threshold = consensus_threshold_seconds
 		self.new_transcription()
 
 	def new_transcription(self):
@@ -35,19 +35,21 @@ class ChunkedTranscription:
 		self.living_audio += audio_segment
 		
 		if len(self.living_audio) > self.consensus_threshold:
-			self.start_time = random.randint(0, self.previous_consensus_time // 2)
+			self.start_time = random.random() * self.previous_consensus_time / 2
 			self.living_audio = self.living_audio[self.start_time:]
 
 		# Transcribe using the tiny model
 		result_tiny = self.tiny_model.transcribe(self.living_audio)
 		
 		# Initialize RangeHeatMap
-		heat_map = RangeHeatMap()
+		print(json.dumps(result_tiny, indent=4))
+		print(self.heat_map.ranges)
 		for segment in result_tiny['segments']:
-			heat_map.append_segment(self._get_segment_time(segment))
+			self.heat_map.append_segment(self._get_segment_time(segment))
+		print(self.heat_map.ranges)
 		
-		if self._reached_consensus():
-			consensus_time = self._get_consensus_time(result_tiny['segments'])
+		consensus_time = self._reached_consensus()
+		if consensus_time > 0:
 			self.previous_consensus_time = consensus_time
 
 			# Transcribe the accumulated living audio using the large model
@@ -56,7 +58,7 @@ class ChunkedTranscription:
 			# Update fixed transcription
 			self.fixed_transcription += result_large['text']
 
-			self.living_audio = self.living_audio[consensus_time:]
+			self.living_audio = self.living_audio[consensus_time*1000:]
 			self.heat_map.ranges.clear()
 
 			return TranscriptionState(fixed_transcription=self.fixed_transcription, 
@@ -81,7 +83,7 @@ class ChunkedTranscription:
 		return state
 
 	def _reached_consensus(self, overlaping_transciptions_required=2) -> bool:
-		range_overlaps = self.heat_map.get_overlapping_ranges(self.living_audio.duration_seconds*1000)
+		range_overlaps = self.heat_map.get_overlapping_ranges(self.living_audio.duration_seconds)
 		print(range_overlaps)
 		if len(range_overlaps) > 1:
 			sufficent_overlap = False
@@ -90,17 +92,18 @@ class ChunkedTranscription:
 					sufficent_overlap = True
 			
 			if sufficent_overlap:
-				if range_overlaps[-1].length() > self.consensus_threshold:
-					return True
-		return False
+				l = range_overlaps[-1].length()
+				if l > self.consensus_threshold:
+					return min(l, self.consensus_threshold) / 2
+		return 0
 
-	def _get_consensus_time(self, segments) -> int:
+	def _get_consensus_time(self, segments) -> float:
 		_, last_segment_end = self._get_segment_time(segments[-2])
 		next_segment_start, _ = self._get_segment_time(segments[-1])
-		consensus_time = (last_segment_end + next_segment_start) // 2
+		consensus_time = (last_segment_end + next_segment_start) / 2
 		return min(consensus_time, self.consensus_threshold)
 
-	def _get_segment_time(self, segment) -> (int, int):
+	def _get_segment_time(self, segment) -> (float, float):
 		# Adjust the start and end times by the start time
 		start = segment['start'] + self.start_time
 		end = segment['end'] + self.start_time
