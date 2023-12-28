db0b999c6b85ab3730f2810bbcc6b509cc3c1c57
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-12-28 08:28:31 -0500

    Messages log caller info (untested)

---
+++ AbstractAI/ConversationModel/MessageSources/CallerInfo.py
@@ -0,0 +1,16 @@
+from AbstractAI.ConversationModel.ModelBase import *
+from AbstractAI.Helpers.log_caller_info import log_caller_info
+
+@ConversationDATA(generated_id_type=ID_Type.HASHID)
+class CallerInfo:
+	module_name: str
+	function_name: str
+	class_name: str
+	instance_id: str
+	file_path: str
+	git_commit: str
+	
+	@staticmethod
+	def get_caller_info():
+		caller_info = log_caller_info()
+		return CallerInfo(**caller_info)
--- AbstractAI/ConversationModel/MessageSources/MessageSource.py
+++ AbstractAI/ConversationModel/MessageSources/MessageSource.py
@@ -1,4 +1,5 @@
 from AbstractAI.ConversationModel.ModelBase import *
+from AbstractAI.ConversationModel.MessageSources.CallerInfo import CallerInfo
 
 @ConversationDATA
 class MessageSource:
@@ -8,3 +9,5 @@ class MessageSource:
     These will contain information about where a message came from.
     """
     system_message: bool = field(default=False, kw_only=True)
+    
+    caller_source: CallerInfo = field(default_factory=CallerInfo.get_caller_info, kw_only=True)
