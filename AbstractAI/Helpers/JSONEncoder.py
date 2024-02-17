2ebfe1c3c0f282bec27b8c0a5d45f9006d7c24d5
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2024-02-17 18:01:24 -0500

    + torch.dtype json serialization

--- AbstractAI/Helpers/JSONEncoder.py
+++ AbstractAI/Helpers/JSONEncoder.py
@@ -1,6 +1,7 @@
 import json
 from enum import Enum
 from datetime import datetime
+from torch import dtype
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -11,4 +12,6 @@ class JSONEncoder(json.JSONEncoder):
                 return obj.strftime("%Y-%m-%d %H:%M:%S.%f %z")
             else:
                 return obj.strftime("%Y-%m-%d %H:%M:%S.%f")
+        if isinstance(obj, dtype):
+            return f"<dtype>(is_floating_point:is_floating_point={obj.is_floating_point}, is_complex={obj.is_complex}, is_signed={obj.is_signed}, itemsize={obj.itemsize})"
         return json.JSONEncoder.default(self, obj)
