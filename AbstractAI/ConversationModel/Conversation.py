c0d838a3c8e8b33699dda23323ceb66846305111
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2024-03-02 15:04:05 -0500

    alternates redesign

--- AbstractAI/ConversationModel/Conversation.py
+++ AbstractAI/ConversationModel/Conversation.py
@@ -4,7 +4,7 @@ from .MessageSequence import MessageSequence
 from .Message import Message
 
 from datetime import datetime
-from typing import Callable, List
+from typing import Callable, List, Union
 
 @ConversationDATA(excluded_fields=["all_message_sequences"])
 class Conversation:
@@ -22,11 +22,7 @@ class Conversation:
 	
 	@property
 	def all_message_sequences(self) -> List[MessageSequence]:
-		if not hasattr(self, "_all_message_sequences"):
-			if self.message_sequence is not None:
-				self._all_message_sequences = [self.message_sequence]
-			else:
-				self._all_message_sequences = []
+		self._ensure_all_message_sequences()
 		return self._all_message_sequences
 	
 	def __post_init__(self):
@@ -37,9 +33,10 @@ class Conversation:
 		self.conversation_changed.connect(conversation_changed)
 		
 	def new_message_sequence(self):
+		self._ensure_all_message_sequences()
 		self.message_sequence = self.message_sequence.copy()
 		self.message_sequence.conversation = self
-		self.all_message_sequences.append(self.message_sequence)
+		self._all_message_sequences.append(self.message_sequence)
 		
 	def add_message(self, message:Message):
 		self.new_message_sequence()
@@ -57,17 +54,30 @@ class Conversation:
 		self.new_message_sequence()
 		self.message_sequence.replace_message(old_message, new_message, keeping_latter)
 	
-	def alternates(self, message:Message) -> List[MessageSequence]:
+	def alternates(self, message:Union[Message,int]) -> List[MessageSequence]:
 		'''
 		Returns a list of MessageSequences that have the same messages as
-		conversation.message_sequence up to message.
+		conversation.message_sequence up to message (inclusively).
 		
-		If message is None, returns a list of MessageSequences that have
-		the same messages as conversation.message_sequence up to the last
-		message.
+		If message is None or less than 0, returns all_message_sequences.
+		
+		If message is an integer, it is used as an index into message_sequence.
+		If message is greater than the length of message_sequence, all message
+		sequences that start with the contents of message_sequence are returned.
 		'''
+		if message is None:
+			return self.all_message_sequences
+		
+		if isinstance(message, int):
+			if message < 0:
+				return self.all_message_sequences
+			elif message < len(self.message_sequence.messages):
+				message = self.message_sequence.messages[message]
+			else:
+				message = self.message_sequence.messages[-1]
+		
 		messages_to_match = [msg.auto_id for msg in self.message_sequence.messages]
-		if message is not None and message.auto_id in messages_to_match:
+		if message.auto_id in messages_to_match:
 			messages_to_match = messages_to_match[:messages_to_match.index(message.auto_id)+1]
 		
 		alternates = [
@@ -76,4 +86,12 @@ class Conversation:
 				ms.messages[:len(messages_to_match)]
 				] == messages_to_match
 		]
+		
 		return alternates
+	
+	def _ensure_all_message_sequences(self):
+		if not hasattr(self, "_all_message_sequences"):
+			if self.message_sequence is not None:
+				self._all_message_sequences = [self.message_sequence]
+			else:
+				self._all_message_sequences = []
