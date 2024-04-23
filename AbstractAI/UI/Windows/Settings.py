589135e12aa7133466ec379bf6489b7bd974d90f
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2024-04-23 15:25:46 -0400

    Added enum control + derived_type field

--- AbstractAI/UI/Windows/Settings.py
+++ AbstractAI/UI/Windows/Settings.py
@@ -6,11 +6,15 @@ from dataclasses import dataclass
 
 from abc import ABC, abstractmethod
 from typing import Any, Type, TypeVar, Dict, Generic, List
+from enum import Enum
 import re
 T = TypeVar('T')
 
 class TypedControl(Generic[T]):
 	valueChanged = pyqtSignal()
+	
+	def __init__(self, derived_type: Type[T] = None):
+		self.derived_type = derived_type if derived_type is not None else T
 	
 	@property
 	def value(self) -> T:
@@ -40,8 +44,15 @@ class TypedControls:
 		cls._registry[type_] = control_type
 
 	@classmethod
-	def get_control(cls, type_: Type) -> Type[TypedControl]:
-		return cls._registry.get(type_)
+	def get_control(self, type_: Type) -> Type[TypedControl]:
+		control_type = self._registry.get(type_, None)
+		if control_type is not None:
+			return control_type
+		
+		for value_type, control_type in self._registry.items():
+			if issubclass(type_, value_type):
+				return control_type
+		raise ValueError(f"Unsupported type {type_.__name__}")
 	
 	def __call__(self, type):
 		def decorator(control_type):
@@ -52,8 +63,9 @@ class TypedControls:
 TypedControls = TypedControls()
 
 class NumberControl(TypedControl[T], QLineEdit):
-	def __init__(self):
-		super().__init__()
+	def __init__(self, derived_type: Type[T] = None):
+		QLineEdit.__init__(self)
+		TypedControl.__init__(self, derived_type=derived_type)
 		self._number_value:T = 0
 		self.editingFinished.connect(self._validate_and_set)
 	
@@ -99,8 +111,9 @@ class FloatControl(NumberControl[float]):
 
 @TypedControls(bool)
 class BoolControl(QCheckBox, TypedControl[bool]):
-	def __init__(self):
-		super().__init__()
+	def __init__(self, derived_type: Type[T] = None):
+		QCheckBox.__init__(self)
+		TypedControl.__init__(self, derived_type=derived_type)
 		self.stateChanged.connect(self.valueChanged.emit)
 
 	def _get_value(self) -> bool:
@@ -111,8 +124,9 @@ class BoolControl(QCheckBox, TypedControl[bool]):
 		
 @TypedControls(str)
 class StrControl(QLineEdit, TypedControl[str]):
-	def __init__(self):
-		super().__init__()
+	def __init__(self, derived_type: Type[T] = None):
+		QLineEdit.__init__(self)
+		TypedControl.__init__(self, derived_type=derived_type)
 		self.textChanged.connect(self.valueChanged.emit)
 		
 	def _get_value(self) -> str:
@@ -123,8 +137,9 @@ class StrControl(QLineEdit, TypedControl[str]):
 
 @TypedControls(list)
 class StrListControl(QLineEdit, TypedControl[List[str]]):
-	def __init__(self):
-		super().__init__()
+	def __init__(self, derived_type: Type[T] = None):
+		QLineEdit.__init__(self)
+		TypedControl.__init__(self, derived_type=derived_type)
 		self.textChanged.connect(self.valueChanged.emit)
 
 	def _get_value(self) -> List[str]:
@@ -132,7 +147,32 @@ class StrListControl(QLineEdit, TypedControl[List[str]]):
 	
 	def _set_value(self, value: List[str]) -> None:
 		self.setText(",".join([str(item) for item in value]))
+		
+@TypedControls(Enum)
+class EnumControl(QComboBox, TypedControl[Enum]):
+	def __init__(self, derived_type: Type[T] = None):
+		QComboBox.__init__(self)
+		TypedControl.__init__(self, derived_type=derived_type)
+		self.currentTextChanged.connect(self.valueChanged.emit)
 
+	def _get_value(self) -> Enum:
+		for enum_value in self.derived_type:
+			if enum_value.name == self.currentText():
+				return enum_value
+		return None
+
+	def _set_value(self, value: Enum) -> None:
+		if not isinstance(value, Enum):
+			raise ValueError("Value must be an Enum")
+		if self.currentText != value.name:
+			self.repopulate(value.__class__)
+			self.setCurrentText(value.name)
+
+	def repopulate(self, enum_type: Type[Enum]):
+		self.clear()
+		for enum_value in enum_type:
+			self.addItem(enum_value.name)
+			
 class SettingItem:
 	def __init__(self, model, path, view=None):
 		self.model = model
@@ -204,10 +244,11 @@ class SettingsWindow(QWidget):
 		if model is not None and hasattr(model, "__annotations__"):
 			for field_name, field_type in model.__annotations__.items():
 				field_value = getattr(model, field_name)
-				control_type = TypedControls.get_control(field_type)
-				if control_type is None:
-					raise ValueError(f"Unsupported type {field_type.__name__} for field {field_name}")
-				control = control_type()
+				try:
+					control_type = TypedControls.get_control(field_type)
+				except ValueError:
+					continue
+				control = control_type(field_type)
 				control.value = field_value
 				def change_value(control=control, model=model, field_name=field_name):
 					setattr(model, field_name, control.value)
@@ -226,7 +267,12 @@ class SettingsWindow(QWidget):
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
 	app.setStyle("Fusion")
-
+	
+	class myEnum(Enum):
+		one = 1
+		two = 2
+		three = 3
+		
 	@dataclass
 	class Model1:
 		a: int
@@ -237,9 +283,10 @@ if __name__ == "__main__":
 	class Model2:
 		d: list
 		e: int
+		f: myEnum
 
 	model1 = Model1(42, "hello world", True)
-	model2 = Model2([1,2,3], 42)
+	model2 = Model2([1,2,3], 42, myEnum.two)
 
 	setting_items = [SettingItem(model1, "Items/Model1"), SettingItem(model2, "Items/Model2")]
 
