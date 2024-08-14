b3c1fc2731353c0a2e43d197cd53645f59bb0945
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2024-08-14 13:54:58 -0400

    wip picker

--- AbstractAI/UI/ChatViews/ConversationPicker.py
+++ AbstractAI/UI/ChatViews/ConversationPicker.py
@@ -11,13 +11,15 @@ class ConversationButton(QPushButton):
 		self.setToolTip(conversation.description)
 
 class ConversationPicker(QWidget):
-	def __init__(self):
+	def __init__(self, button_height: int = 25):
 		super().__init__()
+		self.button_height = button_height
 		self.init_ui()
 		Context.context_changed.connect(self.update_buttons)
 		self.update_buttons()
 
 	def init_ui(self):
+		self.setStyleSheet("background-color: 255,255,255;")
 		main_layout = QHBoxLayout(self)
 		main_layout.setContentsMargins(0, 0, 0, 0)
 
@@ -30,18 +32,19 @@ class ConversationPicker(QWidget):
 		self.layout = QHBoxLayout(self.content_widget)
 		self.layout.setSpacing(10)  # Space between conversation groups
 		self.layout.setContentsMargins(0, 0, 0, 0)
-
+		self.layout.setAlignment(Qt.AlignLeft)  # Align buttons to the left
+		self.scroll_area.setFrameShape(QScrollArea.NoFrame)
 		self.scroll_area.setWidget(self.content_widget)
 		main_layout.addWidget(self.scroll_area)
 
-		self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
+		self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 
 	def update_buttons(self):
 		# Clear existing buttons
-		for i in reversed(range(self.layout.count())):
-			widget = self.layout.itemAt(i).widget()
-			if widget:
-				widget.deleteLater()
+		while self.layout.count():
+			child = self.layout.takeAt(0)
+			if child.widget():
+				child.widget().deleteLater()
 
 		# Create new buttons for each active conversation
 		for conversation in Context.active_conversations:
@@ -51,21 +54,21 @@ class ConversationPicker(QWidget):
 			group_layout.setContentsMargins(0, 0, 0, 0)
 
 			button = ConversationButton(conversation)
+			button.setFixedHeight(self.button_height)
 			button.setChecked(conversation == Context.conversation)
 			button.clicked.connect(self.on_conversation_button_clicked)
 
 			close_button = QPushButton("X")
-			close_button.setFixedWidth(20)
+			close_button.setFixedSize(20, self.button_height)
 			close_button.clicked.connect(lambda _, c=conversation: self.close_conversation(c))
 
 			group_layout.addWidget(button)
 			group_layout.addWidget(close_button)
 
 			self.layout.addWidget(group)
-		self.layout.addStretch()
-		self.content_widget.adjustSize()
-		self.updateGeometry()
-		self.layout.update()
+
+		self.layout.addStretch(1)  # Add stretch at the end to keep buttons left-aligned
+		self.adjustSize()
 		self.update()
 
 	def on_conversation_button_clicked(self):
@@ -75,15 +78,35 @@ class ConversationPicker(QWidget):
 			self.update_buttons()
 
 	def close_conversation(self, conversation: Conversation):
-		index = Context.active_conversations.index(conversation)
-		Context.active_conversations.remove(conversation)
+		if conversation in Context.active_conversations:
+			index = Context.active_conversations.index(conversation)
+			Context.active_conversations.remove(conversation)
 
-		if conversation == Context.conversation:
-			if index < len(Context.active_conversations):
-				Context.conversation = Context.active_conversations[index]
-			elif index > 0:
-				Context.conversation = Context.active_conversations[index - 1]
-			else:
-				Context.conversation = None
+			if conversation == Context.conversation:
+				if Context.active_conversations:
+					Context.conversation = Context.active_conversations[min(index, len(Context.active_conversations) - 1)]
+				else:
+					Context.conversation = None
 
-		Context.context_changed()
+			Context.context_changed()
+
+	def resizeEvent(self, event):
+		super().resizeEvent(event)
+		self.updateGeometry()
+
+	def sizeHint(self):
+		width = self.minimumWidth()
+		height = self.button_height
+		if self.scroll_area.horizontalScrollBar().isVisible():
+			height += self.scroll_area.horizontalScrollBar().height()
+		return QSize(width, height)
+
+	def minimumSizeHint(self):
+		return self.sizeHint()
+
+	def minimumWidth(self):
+		if self.layout.count() > 0:
+			first_item = self.layout.itemAt(0)
+			if first_item and first_item.widget():
+				return first_item.widget().sizeHint().width()
+		return 0
